bb10fff047ae8e19f191aa62b0e83933dc66462f
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2018-11-12 16:59:37 -0800

    commit 3

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Email Form</title>
-	<link href="https://fonts.googleapis.com/css?family=Lato:400,700|PT+Serif|Yanone+Kaffeesatz:700" rel="stylesheet">
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-	<h1>Email Form</h1>
-	<form>
-		<input id="email" type="email" placeholder="Email Address">
-		<button type="submit">Subscribe</button>
-		<p id="message"></p>
-	</form>
+var emailInputField = document.getElementById('email');
 
-	<script src="main.js"></script>
-</body>
-</html>
+emailInputField.addEventListener('blur', checkEmailInput);
+
+function checkEmailInput () {
+
+    if (emailInputField.value.length === 0) {
+        document.getElementById('message').innerText = 'Please enter an email address.'
+        emailInputField.className = 'error';
+    } else {
+        document.getElementById('message').innerText = '';
+        emailInputField.className = '';
+    }
+
+}
